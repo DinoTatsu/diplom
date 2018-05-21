830ebd5587ec973b164e768723aa0c89c6069125
dino <dino.tatsu@gmail.com>
2018-05-21 06:39:09 +0600

    add main

---
+++ kstumd/main/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+    url(r'', TemplateView.as_view(template_name="main.html")),
+]
+
+
+
+
